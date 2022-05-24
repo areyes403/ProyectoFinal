a9624a5b140dc3735b658b84f668a32122f7fe90
areyes403 <areyes403>
2022-05-24 10:06:06 -0500

    installed firebase

--- ProyectoFinal/ViewController.swift
+++ ProyectoFinal/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfUser: UITextField!
+    @IBOutlet weak var txtBienvenida: CLTypingLabel!
+    @IBOutlet weak var tfPassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        txtBienvenida.text = "Bienvenidos al instituto tecnologico de morelia"
+        txtBienvenida.onTypingAnimationFinished={
+            self.txtBienvenida.textColor = .brown
+        }
     }
 
-
+    @IBAction func btnLogin(_ sender: Any) {
+    }
+    
 }
 
